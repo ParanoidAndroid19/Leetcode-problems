a5207ffc39e161e6b67f5a4d5339428ca3f456dd
ParanoidAndroid19 <simranbhake.sb@gmail.com>
2022-09-07 23:36:59 -0500

    dp

--- decodeWays(medium).py
+++ decodeWays(medium).py
@@ -46,3 +46,74 @@ def recur(s):
     
 
 print(numDecodings("11106"))
+
+
+# Approach 2: Bottom up dp
+
+def numDecodings(self, s: str) -> int:
+    # remember you don't even have to decode it, just count the number of ways
+    # this is similar to word break
+    # here at every point there is an option, either you can condsider 1 char or 2 char str
+    # since all alphabets have at most 2 char len str code
+    
+    # def isValid to check if we are not considering thing like "06" and num <= 26
+    
+    # or a dp sol
+    # "1. 2"
+    # [1, 2] (final arr[-1] +1 if entire str can be considered as valid code)
+    
+    # "2  2  6"
+    # [1, 2, 3]
+    
+    # "0. 6"
+    # [0, 0]
+    
+    # "1. 1. 1  0. 6" (if zero always join it with preceeding num?)
+    # [1, 2, 2, 2, 2]
+    
+    # dp[i-1] + 1(if entire str[i-1:i+1] is valid code)
+    
+    # soooo close
+    
+    #s    2  1. 0  1
+    # [1, 1, 2, 1, 1]
+    
+    # so dp[i] would contain num of ways to decode the string s[:i] 
+    # technically till s[i-1]
+    dp = [0 for _ in range(len(s)+1)]
+    dp[0] = 1
+    
+    if s[0] != "0":
+        dp[1] = 1
+        
+    for i in range(2, len(dp)):
+        
+        # for ways to decode single digit, that would be dp leading to it
+        if s[i-1] != "0":
+            dp[i] = dp[i-1]
+            
+        doubleDigit = int(s[i-2:i])
+        
+        # no. of ways to decode two digits, that would be the dp leading to the 1st digit
+        if 10 <= doubleDigit and doubleDigit <= 26:
+            dp[i] = dp[i] + dp[i-2]
+            
+    return dp[-1]
+    
+#         dp = [0 for _ in range(len(s))]
+    
+#         if s[0] != "0":
+#             dp[0] = 1
+        
+#         for i in range(1, len(s)):
+#             print(s[i])
+#             dp[i] = dp[i-1]
+        
+#             two = int(s[i-1:i+1])
+        
+#             if 11 <= two and two <= 26:
+#                 dp[i] = dp[i] + 1
+    
+#         print('dp', dp)
+            
+#         return dp[-1]
